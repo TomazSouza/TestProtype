80ede4e07bd9135b1f12e8210ed48d7afaec2b97
TomazSouza <48397858+TomazSouza@users.noreply.github.com>
2022-06-23 13:46:26 -0300

    add build version in gradle

--- app/src/main/java/br/com/promobit/testprotype/MainActivity.java
+++ app/src/main/java/br/com/promobit/testprotype/MainActivity.java
@@ -1,8 +1,13 @@
 package br.com.promobit.testprotype;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SwitchMaterial sw1 = findViewById(R.id.switch1);
+        SwitchMaterial sw2 = findViewById(R.id.switch2);
+
+        sw1.setOnCheckedChangeListener((compoundButton, checked) -> {
+            Toast.makeText(getApplicationContext(), "Switch1 " + checked, Toast.LENGTH_SHORT).show();
+
+            sw2.setEnabled(!checked);
+        });
+
+
+        sw2.setOnCheckedChangeListener((compoundButton, checked) -> {
+            Toast.makeText(getApplicationContext(), "Switch2 " + checked, Toast.LENGTH_SHORT).show();
+            sw1.setEnabled(!checked);
+        });
     }
 }
