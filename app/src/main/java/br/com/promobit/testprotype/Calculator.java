35a0d858c1c7fdedaef322b5c768df64de1f38d8
TomazSouza <48397858+TomazSouza@users.noreply.github.com>
2022-06-22 22:12:06 -0300

    first commit

---
+++ app/src/main/java/br/com/promobit/testprotype/Calculator.java
@@ -0,0 +1,14 @@
+package br.com.promobit.testprotype;
+
+public class Calculator {
+
+    private int num1;
+    private int num2;
+
+    public Calculator(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+
+}
---
+++ app/src/test/java/br/com/promobit/testprotype/ExampleUnitTest.java
@@ -0,0 +1,25 @@
+package br.com.promobit.testprotype;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void checkAllOperations() {
+//        Calculator calculator = new Calculator(6, 2);
+
+
+    }
+}
